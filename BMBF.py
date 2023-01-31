62973502966bd72672630ef55e12bb75a86d5bc7
Muhammad Basari <107491771+Basari-ID@users.noreply.github.com>
2023-01-31 15:49:42 +0700

    Update BMBF.py

--- BMBF.py
+++ BMBF.py
@@ -1,10 +1,11 @@
 import time
-from rich.panel import Panel as basari
-from rich import print as ganteng
+from rich.panel import Panel as ganteng
+from rich import print as basari
 try:
 	warna_kolor = "#00C8FF"
 except FileNotFoundError:
 	warna_kolor = "#00C8FF"
-ganteng(basari(f'[bold green] mohon maaf bagi para pengguna script bmbf karena script ini\n tidak bisa digunakan untuk sementara waktu tolong dimaafkan',width=70,title=f"[bold white] ◇ PEMBERITAHUAN ◇ ",style=f"{warna_kolor}"))
-time.sleep(2)
+os.system('clear')
+basari(ganteng(f'[bold green] mohon maaf bagi para pengguna script bmbf karena script ini\n tidak bisa digunakan untuk sementara waktu tolong dimaafkan',width=70,title=f"[bold white] ◇ PEMBERITAHUAN ◇ ",style=f"{warna_kolor}"))
+time.sleep(4)
 exit()
